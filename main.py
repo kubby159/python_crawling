4d801453472e8c6f026d14d702f4989133828d5c
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-06-24 23:10:13 +0900

    크롤링 연습

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ print(arr)
 
 #position
 #pos
-s = '제 생일은 9월 입니다.'
-pos = s.find("생일은 ")
-pos+=4
-print(s[pos:pos+1])
+s = '제 생일은 10월 입니다.'
+# pos = s.find("생일은 ")
+# pos+=4
+# print(s[pos:pos+1])
+
+
+arr = s.split('생일은 ')
+s = arr[1]
+print(s.split('월')[0])
+
+s = '제 생일은 10월 입니다.'
+bd = s.split('생일은 ')[1].split('월')[0]
+print(bd)
