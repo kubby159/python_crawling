47ff0e953483632c203e9a2f9307ffa9b0a19c25
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-06-25 01:01:30 +0900

    정규식을 이용한 크롤링

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import requests as req
-
-res = req.get('https://finance.naver.com/marketindex/?tabSel=exchange#tab_section')
-
-html = res.text
+import re
 
 
-result = html.split('<span class="value">')[1].split('</span>')[0]
-print(result)
+# A B C F
+
+s = "이 영화는 F등급 입니다"
+# print(s.split('이 영화는')[1].split('등급')[0])
+# print(re.match(r'이 영화는 [ABCF]등급 입니다',s))
+
+# regex 사용
+# print(s.split('이 영화는 ')[1].split('등급')[0])
+print(re.findall(r'이 (..)는 ([ABCF])등급 입니다',s))
