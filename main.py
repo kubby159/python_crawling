bf5eaef6000da80ef8e31db4743f540c93c30130
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-06 16:00:08 +0900

    beautifulsoup

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
+from ctypes import wstring_at
+from bs4 import BeautifulSoup as BS
 import requests as req
-import re
-
-
-
-url = 'https://finance.naver.com/marketindex/?tabSel=exchange#tab_section'
-
+ 
+url = 'https://finance.naver.com/marketindex/exchangeList.naver'
 res = req.get(url)
-body = res.text
-
-# DOT (.) 은 엔터를 미포함.
-# re.DoTALL : 엔터를 포함한다.
-# ? 를 이용해서 가장 좁은 범위의 데이터를 가져옴.
-r = re.compile(r'h_lst.*?blind">(.*?)</span>.*?value">(.*?)</', re.DOTALL) # 정규식표현을 작성한다.
-
-captures = r.findall(body)
-
-print(captures)
-
-print('----------')
-print('환율 계산기')
-print('----------')
+soup = BS(res.text,'html.parser')
 
 
-for c in captures:
-    print(f'{c[0]} : {c[1]}')
+tds = soup.find_all('td')
 
+names = []
+
+for td in tds:
+    if len(td.find_all('a')) == 0:
+        continue
+    names.append(td.get_text(strip=True))
+
+prices = []
+for td in tds:
+    if 'class'in td.attrs:
+        if 'sale' in td.attrs["class"]:
+            prices.append(td.get_text(strip=True))
 
 
 
+    
 
-print()
-usd = float(captures[0][1].replace(',',''))
-won = int(input("달러로 바꾸길 원하는 금액(원)을 입력해주세요."))
-
-dollar = int(won/usd)  
-print(f'{dollar} 달러 환전되었습니다.')
+for idx in range(0,len(names)):
+    print(f'{names[idx]} : {prices[idx]}')
