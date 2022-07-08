8d34882b1ca22f935191c5d7641c6f3c540ebd81
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-08 21:21:40 +0900

    고급한정자 연습

--- main.py
+++ main.py
@@ -1,46 +1,18 @@
-from ctypes import wstring_at
 from bs4 import BeautifulSoup as BS
-import requests as req
- 
-url = 'https://finance.naver.com/marketindex/exchangeList.naver'
-res = req.get(url)
-soup = BS(res.text,'html.parser')
+
+html = '''
+
+<html >
+
+  <body>
+    <div>안녕하세요</div>
+  </body>
+</html>
 
 
-tds = soup.find_all('td')
+'''
 
-names = []
+soup = BS(html,'html.parser')
 
-for td in tds:
-    if len(td.find_all('a')) == 0:
-        continue
-    names.append(td.get_text(strip=True))
-
-prices = []
-for td in tds:
-    if 'class'in td.attrs:
-        if 'sale' in td.attrs["class"]:
-            prices.append(td.get_text(strip=True))
-
-
-
-    
-
-# for idx in range(0,len(names)):
-#     print(f'{names[idx]} : {prices[idx]}')
-#
-
-names = []
-
-for td in soup.select('td.tit'):
-    names.append(td.get_text(strip=True))
-
-
-prices = []
-
-for td in soup.select('td.sale'):
-    prices.append(td.get_text(strip=True))
-
-
-print(names)
-print(prices)
+div = soup.select('div')
+print(div[0].get_text(strip=True))
