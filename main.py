95923f9abc2beb950d6c7602580ae994099d6441
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-08 21:56:00 +0900

    크롤링 연습

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+import bs4
+
+
 from bs4 import BeautifulSoup as BS
-
-html = '''
-
-<html >
-
-  <body>
-    <div>안녕하세요</div>
-  </body>
-</html>
+import requests as req
 
 
-'''
+url = 'https://search.shopping.naver.com/search/all?query=%EC%95%84%EC%9D%B4%ED%8F%B0%20%EC%BC%80%EC%97%90%EC%8A%A4&cat_id=&frm=NVSHATC'
+res = req.get(url)
+soup = BS(res.text,'html.parser')
 
-soup = BS(html,'html.parser')
+arr = soup.select('ul.list_basis div> a:first-child[title]')
 
-div = soup.select('div')
-print(div[0].get_text(strip=True))
+for a in arr:
+    print(a.get_text(strip=True))
