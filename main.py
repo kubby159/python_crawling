88ec6f8f3b1251d83b6c8685967e8dfe5edb4406
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-10 00:15:30 +0900

    revised

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ soup = BS(res.text, 'html.parser')
 stocks =[stock.get_text(strip=True) for stock in soup.select('a.tltle')]
 stock_price = [price.get_text(strip=True) for price in soup.select('tr td.number:nth-child(4)')]
 stock_ratio = [price.get_text(strip=True) for price in soup.select('tr td.number:nth-child(6)')]
-print(stock_price)
+
 for idx in range(len(stocks)):
     print(f'{stocks[idx]} : {stock_price[idx]} {stock_ratio[idx]}')
 
