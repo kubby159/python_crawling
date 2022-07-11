2a1572122737912b1d28ffb9bfa58dde757af008
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-11 17:52:49 +0900

    이미지 업로드

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import requests as req
 
+url = ('https://api.imgur.com/3/image?client_id=546c25a59c58ad7')
 
-res = req.get('https://webhook.site/a36713b2-b32b-4e00-8057-cae35ae3cf2d?name=hi',headers={
-    "User-Agent":  "fastcampus/B1"
+# image.jpeg
+# f =open('image.jpeg', 'rb')
+# img = f.read()
+# f.close()
 
+with open('image.jpeg','rb') as f:
+    img = f.read()
+
+res = req.post(url,files = {
+    "image": img,
+    "type" : "file",
+    "name" : "image.jpeg"
 })
-print(res)
+
+print(res.text)
+
+
+
+# 200-> 성공
+# 400 -> 보내는 사람 잘못
+# 500 -> 받는 사람 잘못
